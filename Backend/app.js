c7be04ff9b0d8df5a6b1e8edf24384911c4479f4
LukasSemler <semler.l04@htlwienwest.at>
2022-03-08 11:18:30 +0100

    Server Basis

--- Backend/app.js
+++ Backend/app.js
@@ -1,1 +1,28 @@
-console.log("Backend")
+import express from 'express';
+import path from 'path';
+import morgan from 'morgan';
+import helmet from 'helmet';
+
+// require('dotenv').config();
+import { config } from 'dotenv';
+config();
+
+import customerRoutes from './routes/bla.js';
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(helmet());
+
+const dirname = path.resolve();
+
+app.use(express.static(path.join(dirname, 'public')));
+app.use(express.json()); // body parser
+app.use('/customers', customerRoutes);
+app.use('/demo', (req, res) => res.send('Ok'));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
+
+console.log('Server started');
