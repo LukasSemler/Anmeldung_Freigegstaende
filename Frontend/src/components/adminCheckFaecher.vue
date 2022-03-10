1cc62a57535b69e507c123ce64f5166f8a349ee3
LukasSemler <semler.l04@htlwienwest.at>
2022-03-10 14:56:36 +0100

    check fächer fast fertig

--- Frontend/src/components/adminCheckFaecher.vue
+++ Frontend/src/components/adminCheckFaecher.vue
@@ -4,6 +4,13 @@
   <br />
   <div class="flex justify-center">
     <div class="bg-white shadow overflow-hidden sm:rounded-lg w-2/3">
+      <div class="px-4 py-5 sm:px-6 flex justify-center">
+        <img
+          class="h-48 w-96 object-scale-down"
+          crossorigin="anonymous"
+          src="http://localhost:2410/images/asdasd.png"
+        />
+      </div>
       <div class="px-4 py-5 sm:px-6">
         <h3 class="text-lg leading-6 font-medium text-gray-900">TypeScript</h3>
         <p class="mt-1 max-w-2xl text-sm text-gray-500">Von Robert Baumgartner</p>
@@ -24,69 +31,50 @@
             </dd>
           </div>
           <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt class="text-sm font-medium text-gray-500">Benötigte Stunden</dt>
+            <dt class="text-sm font-medium text-gray-500">Jahrgänge</dt>
             <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
               <nav class="space-y-1" aria-label="Sidebar">
                 <a
-                  v-for="item in navigation"
-                  :key="item.name"
-                  :href="item.href"
+                  v-for="item in voraussetzungen"
+                  :key="item"
                   :class="[
-                    item.current ? 'bg-gray-100 text-gray-900' : 'text-gray-600 hover:bg-gray-50',
+                    item ? 'bg-gray-200 text-gray-900' : 'text-gray-600 hover:bg-gray-50',
                     'flex items-center px-3 py-2 text-sm font-medium rounded-md',
                   ]"
-                  :aria-current="item.current ? 'page' : undefined"
+                  :aria-current="item ? 'page' : undefined"
                 >
                   <span class="truncate">
-                    {{ item.name }}
+                    {{ item }}
                   </span>
                 </a>
               </nav>
             </dd>
           </div>
           <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt class="text-sm font-medium text-gray-500">Jahrgänge</dt>
-            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">$120,000</dd>
+            <dt class="text-sm font-medium text-gray-500">Benötigte Stunden</dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">2. Stunden</dd>
           </div>
-          <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt class="text-sm font-medium text-gray-500">About</dt>
-            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa
-              consequat. Excepteur qui ipsum aliquip consequat sint. Sit id mollit nulla mollit
-              nostrud in ea officia proident. Irure nostrud pariatur mollit ad adipisicing
-              reprehenderit deserunt qui eu.
-            </dd>
-          </div>
-          <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt class="text-sm font-medium text-gray-500">Attachments</dt>
-            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              <ul role="list" class="border border-gray-200 rounded-md divide-y divide-gray-200">
-                <li class="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
-                  <div class="w-0 flex-1 flex items-center">
-                    <PaperClipIcon class="flex-shrink-0 h-5 w-5 text-gray-400" aria-hidden="true" />
-                    <span class="ml-2 flex-1 w-0 truncate"> resume_back_end_developer.pdf </span>
-                  </div>
-                  <div class="ml-4 flex-shrink-0">
-                    <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
-                      Download
-                    </a>
-                  </div>
-                </li>
-                <li class="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
-                  <div class="w-0 flex-1 flex items-center">
-                    <PaperClipIcon class="flex-shrink-0 h-5 w-5 text-gray-400" aria-hidden="true" />
-                    <span class="ml-2 flex-1 w-0 truncate">
-                      coverletter_back_end_developer.pdf
-                    </span>
-                  </div>
-                  <div class="ml-4 flex-shrink-0">
-                    <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
-                      Download
-                    </a>
-                  </div>
-                </li>
-              </ul>
-            </dd>
+          <div
+            class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 flex justify-center"
+          >
+            <button
+              @click="anmelden"
+              class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-green-500 hover:bg-green-300 hover:text-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black"
+            >
+              Annehmen
+            </button>
+            <button
+              @click="anmelden"
+              class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-500 hover:text-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black"
+            >
+              Ablehnen
+            </button>
+            <button
+              @click="anmelden"
+              class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 hover:text-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black"
+            >
+              Ändern
+            </button>
           </div>
         </dl>
       </div>
@@ -95,14 +83,5 @@
 </template>
 
 <script setup>
-import { PaperClipIcon } from '@heroicons/vue/solid';
-
-const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
-  { name: 'Documents', href: '#', current: false },
-  { name: 'Reports', href: '#', current: false },
-];
+const voraussetzungen = ['4. Klasse', '5. Klasse'];
 </script>
