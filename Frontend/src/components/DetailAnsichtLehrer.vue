c08b5b9a29066a78e8b4e201d7d40c4ec4a80fab
LukasSemler <semler.l04@htlwienwest.at>
2022-03-25 13:33:25 +0100

    Check Datum Schüler annehmen

--- Frontend/src/components/DetailAnsichtLehrer.vue
+++ Frontend/src/components/DetailAnsichtLehrer.vue
@@ -1,4 +1,70 @@
 <template>
+  <TransitionRoot as="template" :show="showModalWarning">
+    <Dialog as="div" class="fixed z-10 inset-0 overflow-y-auto" @close="showModalWarning = false">
+      <div
+        class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
+      >
+        <TransitionChild
+          as="template"
+          enter="ease-out duration-300"
+          enter-from="opacity-0"
+          enter-to="opacity-100"
+          leave="ease-in duration-200"
+          leave-from="opacity-100"
+          leave-to="opacity-0"
+        >
+          <DialogOverlay class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </TransitionChild>
+
+        <!-- This element is to trick the browser into centering the modal contents. -->
+        <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true"
+          >&#8203;</span
+        >
+        <TransitionChild
+          as="template"
+          enter="ease-out duration-300"
+          enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          enter-to="opacity-100 translate-y-0 sm:scale-100"
+          leave="ease-in duration-200"
+          leave-from="opacity-100 translate-y-0 sm:scale-100"
+          leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+        >
+          <div
+            class="relative inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6"
+          >
+            <div>
+              <div
+                class="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100"
+              >
+                <ExclamationIcon class="h-6 w-6 text-orange-600" aria-hidden="true" />
+              </div>
+              <div class="mt-3 text-center sm:mt-5">
+                <DialogTitle as="h3" class="text-lg leading-6 font-medium text-gray-900">
+                  Warning
+                </DialogTitle>
+                <div class="mt-2">
+                  <p class="text-sm text-gray-500">
+                    Da die Frist zum Anmelden noch nicht abgelaufen ist, können noch keine
+                    Schüler*innen angenommen oder abgelehnt werden
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="mt-5 sm:mt-6">
+              <button
+                type="button"
+                class="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-orange-600 text-base font-medium text-white hover:bg-orange-500 focus:outline-none sm:text-sm"
+                @click="showModalWarning = false"
+              >
+                Akzeptieren
+              </button>
+            </div>
+          </div>
+        </TransitionChild>
+      </div>
+    </Dialog>
+  </TransitionRoot>
+
   <div>
     <div class="h-full flex">
       <!-- Static sidebar for desktop -->
@@ -204,12 +270,24 @@
 
 <script setup>
 import { MailIcon } from '@heroicons/vue/solid';
+import {
+  Dialog,
+  DialogOverlay,
+  DialogTitle,
+  TransitionChild,
+  TransitionRoot,
+} from '@headlessui/vue';
+import { ExclamationIcon } from '@heroicons/vue/outline';
 import { onMounted, ref } from 'vue';
 import Store from '../composables/Store.js';
 import axios from 'axios';
+import moment from 'moment';
 
 let freifachRef = ref({});
 let schueler = ref([]);
+let showModalWarning = ref(false);
+let fristAnmelden = ref(null);
+let erg = ref(null);
 
 //Schüler die im Freifach inkludiert sind
 let angenommen = ref([]);
@@ -218,6 +296,21 @@ onMounted(async () => {
   try {
     const freifach = JSON.parse(localStorage.getItem('detailAnsichtLehrer'));
     freifachRef.value = freifach;
+
+    //TODO Datum checken
+    let aktuellesDatum;
+    //Daten holen
+    fristAnmelden.value = Store.state.fristAnmelden.original;
+    console.log(fristAnmelden.value);
+    aktuellesDatum = new Date();
+
+    //Schauen ob das Datum vor oder nach der Frist ist
+    erg.value = moment(fristAnmelden.value).isBefore(aktuellesDatum);
+
+    if (!erg.value) {
+      console.log('Sie können noch nicht checken');
+      showModalWarning.value = true;
+    }
   } catch (err) {
     console.log(err);
     // console.error('Es wurde kein Freifach gefunden');
@@ -231,27 +324,35 @@ onMounted(async () => {
 });
 
 async function annehmen(s) {
-  const res = await axios.patch(`http://localhost:2410/accepDeclineStudent/${s.s_id}`, {
-    status: 'true',
-    fachID: freifachRef.value.f_id,
-  });
+  if (!erg.value) {
+    showModalWarning.value = true;
+  } else {
+    const res = await axios.patch(`http://localhost:2410/accepDeclineStudent/${s.s_id}`, {
+      status: 'true',
+      fachID: freifachRef.value.f_id,
+    });
 
-  if (res.status == 200) {
-    const gefunden = schueler.value.find((elem) => elem == s);
-    gefunden.status = 'true';
-    angenommen.value.push(gefunden);
-    console.log(gefunden);
+    if (res.status == 200) {
+      const gefunden = schueler.value.find((elem) => elem == s);
+      gefunden.status = 'true';
+      angenommen.value.push(gefunden);
+      console.log(gefunden);
+    }
   }
 }
 
 async function ablehnen(s) {
-  const res = await axios.patch(`http://localhost:2410/accepDeclineStudent/${s.s_id}`, {
-    status: 'false',
-    fachID: freifachRef.value.f_id,
-  });
+  if (!erg.value) {
+    showModalWarning.value = true;
+  } else {
+    const res = await axios.patch(`http://localhost:2410/accepDeclineStudent/${s.s_id}`, {
+      status: 'false',
+      fachID: freifachRef.value.f_id,
+    });
 
-  if (res.status == 200) {
-    schueler.value = schueler.value.filter((elem) => elem != s);
+    if (res.status == 200) {
+      schueler.value = schueler.value.filter((elem) => elem != s);
+    }
   }
 }
 
