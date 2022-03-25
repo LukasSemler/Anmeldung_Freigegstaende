2ca35e4b8c48c6970a6f16f90796f7aa953b707c
Benjamin Stauf <stauf.b03@htlwienwest.at>
2022-03-25 14:35:10 +0100

    Frontend routen abgesichter

--- Frontend/src/composables/Store.js
+++ Frontend/src/composables/Store.js
@@ -12,6 +12,41 @@ const getters = {
   getAktivenUser() {
     return readonly(state.aktiverUser);
   },
+
+  //Returning true oder false, je nachdem was der Aktuelle User ist
+  getSchueler() {
+    try {
+      if (state.aktiverUser.s_id) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch {
+      return false;
+    }
+  },
+  getLehrer() {
+    try {
+      if (state.aktiverUser.isLehrer) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch {
+      return false;
+    }
+  },
+  getAdmin() {
+    try {
+      if (state.aktiverUser.isLehrer && state.aktiverUser.isAdmin) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch {
+      return false;
+    }
+  },
 };
 
 const actions = {
--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -8,6 +8,7 @@ import adminCheckFaecherViewVue from '../views/adminCheckFaecherView.vue';
 import FreifachDetailVue from '../views/FreifachDetail.vue';
 import DetailAnsichtView from '../views/DetailAnsichtView.vue';
 import FAQView from '../views/FAQView.vue';
+import Store from '../composables/Store.js';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,6 +22,13 @@ const router = createRouter({
       path: '/addFach',
       name: 'addFach',
       component: addFachView,
+      beforeEnter: (to, from, next) => {
+        if (Store.getters.getLehrer()) {
+          next();
+        } else {
+          next('/');
+        }
+      },
     },
     {
       path: '/Error404',
@@ -31,16 +39,37 @@ const router = createRouter({
       path: '/Account',
       name: 'account',
       component: AccountView,
+      beforeEnter: (to, from, next) => {
+        if (Store.getters.getAktivenUser()) {
+          next();
+        } else {
+          next('/');
+        }
+      },
     },
     {
       path: '/setFrist',
       name: 'setFrist',
       component: setFristView,
+      beforeEnter: (to, from, next) => {
+        if (Store.getters.getAdmin()) {
+          next();
+        } else {
+          next('/');
+        }
+      },
     },
     {
       path: '/adminCheckFaecher',
       name: 'adminCheckFaecher',
       component: adminCheckFaecherViewVue,
+      beforeEnter: (to, from, next) => {
+        if (Store.getters.getLehrer()) {
+          next();
+        } else {
+          next('/');
+        }
+      },
     },
     {
       path: '/freifachDetail',
@@ -51,6 +80,13 @@ const router = createRouter({
       path: '/freifachDetailLehrer',
       name: 'FreifachDetailLehrer',
       component: DetailAnsichtView,
+      beforeEnter: (to, from, next) => {
+        if (Store.getters.getLehrer()) {
+          next();
+        } else {
+          next('/');
+        }
+      },
     },
     {
       path: '/FAQ',
