4588e997a626ed1e007bc9cde68114ed31e81e29
LukasSemler <semler.l04@htlwienwest.at>
2022-04-18 19:43:25 +0200

    Webuntis PW Modal

--- Frontend/src/components/NavBar.vue
+++ Frontend/src/components/NavBar.vue
@@ -111,6 +111,100 @@
               </button>
             </div>
 
+            <TransitionRoot as="template" :show="WebuntisPasswordFieldShow">
+              <Dialog
+                as="div"
+                class="fixed z-10 inset-0 overflow-y-auto"
+                @close="WebuntisPasswordFieldShow = false"
+              >
+                <div
+                  class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
+                >
+                  <TransitionChild
+                    as="template"
+                    enter="ease-out duration-300"
+                    enter-from="opacity-0"
+                    enter-to="opacity-100"
+                    leave="ease-in duration-200"
+                    leave-from="opacity-100"
+                    leave-to="opacity-0"
+                  >
+                    <DialogOverlay
+                      class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+                    />
+                  </TransitionChild>
+
+                  <!-- This element is to trick the browser into centering the modal contents. -->
+                  <span
+                    class="hidden sm:inline-block sm:align-middle sm:h-screen"
+                    aria-hidden="true"
+                    >&#8203;</span
+                  >
+                  <TransitionChild
+                    as="template"
+                    enter="ease-out duration-300"
+                    enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                    enter-to="opacity-100 translate-y-0 sm:scale-100"
+                    leave="ease-in duration-200"
+                    leave-from="opacity-100 translate-y-0 sm:scale-100"
+                    leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                  >
+                    <div
+                      class="relative inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6"
+                    >
+                      <div>
+                        <div class="mt-2 text-center sm:mt-5">
+                          <DialogTitle as="h1" class="text-lg leading-6 font-bold text-gray-900">
+                            Webuntis Password
+                          </DialogTitle>
+                          <div class="mt-2">
+                            <p class="text-sm text-gray-500">
+                              Fuer die volle Funktionalitaet dieser Webseite zu gewaehleisten, muss
+                              ein Webuntis-Passwort eingegeben werden.
+                            </p>
+
+                            <div>
+                              <label for="email" class="block text-sm font-bold text-gray-700 mt-3"
+                                >Webuntis Passwort</label
+                              >
+                              <div class="mt-1">
+                                <input
+                                  type="Password"
+                                  name="webuntisPassword"
+                                  v-model="WebuntisPasswordFieldInput"
+                                  class="shadow-sm focus:ring-htl_rot focus:border-htl_hellrot block w-full sm:text-sm border-gray-300 rounded-md"
+                                  placeholder="*******"
+                                />
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div
+                        class="mt-5 sm:mt-6 sm:grid sm:grid-cols-2 sm:gap-3 sm:grid-flow-row-dense"
+                      >
+                        <button
+                          type="button"
+                          class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-htl_rot text-base font-medium text-white hover:bg-htl_hellrot sm:col-start-2 sm:text-sm"
+                          @click="anmeldenMitUntisdaten"
+                        >
+                          Senden
+                        </button>
+                        <button
+                          type="button"
+                          class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 sm:mt-0 sm:col-start-1 sm:text-sm"
+                          @click="WebuntisPasswordFieldShow = false"
+                          ref="cancelButtonRef"
+                        >
+                          Cancel
+                        </button>
+                      </div>
+                    </div>
+                  </TransitionChild>
+                </div>
+              </Dialog>
+            </TransitionRoot>
+
             <!-- Button zum anmeldenGoogle anzeigen -->
             <button
               @click="anmeldenGoogle"
@@ -186,7 +280,14 @@ import {
   MenuItem,
   MenuItems,
 } from '@headlessui/vue';
-import { MenuIcon, XIcon, CheckCircleIcon, XCircleIcon } from '@heroicons/vue/outline';
+import {
+  Dialog,
+  DialogOverlay,
+  DialogTitle,
+  TransitionChild,
+  TransitionRoot,
+} from '@headlessui/vue';
+import { MenuIcon, XIcon, XCircleIcon } from '@heroicons/vue/outline';
 
 const store = PiniaStore();
 const googleStore = GoogleStore();
