6f4118a456e6e5f73b103e4f8f3aded7f522ecd6
Benjamin Stauf <stauf.b03@htlwienwest.at>
2022-05-05 20:25:58 +0200

    FooterLinks

--- Frontend/src/components/FooterComp.vue
+++ Frontend/src/components/FooterComp.vue
@@ -3,7 +3,7 @@
     <div class="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
       <nav class="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
         <div v-for="item in navigation.main" :key="item.name" class="px-5 py-2">
-          <a :href="item.href" class="text-base text-gray-500 hover:text-gray-900">
+          <a @click="$router.push(item.href)" class="text-base text-gray-500 hover:text-gray-900 cursor-pointer">
             {{ item.name }}
           </a>
         </div>
@@ -35,7 +35,6 @@ import { defineComponent, h } from 'vue';
 
 const navigation = {
   main: [
-    //TODO Link changen
     { name: 'Home', href: '/' },
     { name: 'Account', href: '/Account' },
     { name: 'Schulwebseite', href: 'https://www.htlwienwest.at/' },
